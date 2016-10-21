237259842c888deddb35ac8fe85cfb2b0865d196
Charlie Barge <artemisc360@gmail.com>
2016-10-21 16:42:25 -0400

    added new assets

---
+++ DEV/ChemistryVR/Assets/FlashCamera.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;  // add to the top
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class FlashCamera : MonoBehaviour {
+	public CanvasGroup flashCanvas;
+	private bool flash = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//flashCanvas = GameObject.Find ("CameraFlash").GetComponent<CanvasGroup> ();
+		if (flash)
+		{
+			flashCanvas.alpha = flashCanvas.alpha - Time.deltaTime;
+			Debug.Log("Flash should happen with alpha " + flashCanvas.alpha);
+			if (flashCanvas.alpha <= 0)
+			{
+				flashCanvas.alpha = 0;
+				flash = false;
+			}
+		}
+
+		//GazeInputModule gazeTest = GameObject.Find ("EventSystem").GetComponent<GazeInputModule> ();
+	}
+
+	public void TakePhoto() {
+		Invoke ("DoIt", 1);
+	}
+
+	public void DoIt() {
+		//flashCanvas = GameObject.Find ("CameraFlash").GetComponent<CanvasGroup> ();
+		// should delay to allow student to move view
+		Debug.Log ("Send to teacher pressed");
+		flash = true;
+		flashCanvas.alpha = 1;
+	}
+}
---
+++ DEV/ChemistryVR/Assets/GoToScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToScene : MonoBehaviour {
+
+	public void LoadScene()
+	{
+		Application.LoadLevel ("ChemistyClass");
+	}
+}
